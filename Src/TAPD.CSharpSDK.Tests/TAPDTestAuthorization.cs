aef78168f5e98e02245f1d7ba2507f27abb2fc54
LaryLawliet <1577075604@qq.com>
2020-03-22 20:25:57 +0800

    --story=1000066 --user=Lawliet 【测试】增加密钥功能单元测试

---
+++ Src/TAPD.CSharpSDK.Tests/TAPDTestAuthorization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TAPD.CSharpSDK.Tests
+{
+    /// <summary>
+    /// 由于保密原因，因此不提供账号和密钥进行测试
+    /// 请向TAPD申请
+    /// </summary>
+    public class TAPDTestAuthorization
+    {
+        /// <summary>
+        /// 测试的帐号
+        /// </summary>
+        public const string API_USER = "null";
+
+        /// <summary>
+        /// 测试的口令
+        /// </summary>
+        public const string API_PASSWORD = "null";
+    }
+}
--- Src/TAPD.CSharpSDK.Tests/TAPD_Http_Test.cs
+++ Src/TAPD.CSharpSDK.Tests/TAPD_Http_Test.cs
@@ -6,12 +6,54 @@ namespace TAPD.CSharpSDK.Tests
     [TestFixture]
     public class TAPD_Http_Test
     {
+        /// <summary>
+        /// 授权编码成功
+        /// </summary>
+        [Test]
+        public void Encode_Authorization_Succeed()
+        {
+            TAPD tapd = new TAPD("api_user","api_password");
+
+            Assert.AreEqual(tapd.authorization, "Basic YXBpX3VzZXI6YXBpX3Bhc3N3b3Jk");
+        }
+
+        /// <summary>
+        /// 请求API成功
+        /// </summary>
         [Test]
         public void Request_API_Succeed()
         {
             var response = TAPDHttp.Request<TAPDResponse<string>>(TAPDHttpAPI.BASE_URL);
 
             Assert.AreEqual(response.status, TAPDHttpStatus.Succeed);
+        }
+
+        /// <summary>
+        /// 请求API授权成功
+        /// 由于保密原因，因此不提供账号和密钥进行测试。
+        /// 请向TAPD申请账号和密钥，不然会导致测试失败。
+        /// </summary>
+        [Test]
+        public void Request_API_Authorized()
+        {
+            TAPD tapd = new TAPD(TAPDTestAuthorization.API_USER, TAPDTestAuthorization.API_PASSWORD);
+
+            var response = tapd.Request<object>("quickstart/testauth");
+
+            Assert.AreEqual(response.status, TAPDHttpStatus.Succeed);
+        }
+
+        /// <summary>
+        /// 请求API没有授权
+        /// </summary>
+        [Test]
+        public void Request_API_Unauthorized()
+        {
+            TAPD tapd = new TAPD("null", "null");
+
+            var response = tapd.Request<string>("quickstart/testauth");
+
+            Assert.AreEqual(response.status, TAPDHttpStatus.Unauthorized);
         }
     }
 }

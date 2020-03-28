d2377877f31a4925830253594753f5609c7fbf4f
LaryLawliet <1577075604@qq.com>
2020-03-28 23:34:07 +0800

    --story=1000069 --user=Lawliet 【功能】增加异常

---
+++ Src/TAPD.CSharpSDK/Exception/TAPDException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TAPD.CSharpSDK
+{
+    public class TAPDException : Exception
+    {
+        public TAPDException(string message) : this(message, null)
+        {
+            
+        }
+
+        public TAPDException(string message, Exception innerException) : base(string.Format("[TAPD]{0}", message), innerException)
+        {
+            
+        }
+    }
+}
---
+++ Src/TAPD.CSharpSDK/Exception/TAPDRequiredParameterMissingException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TAPD.CSharpSDK
+{
+    public class TAPDRequiredParameterMissingException : TAPDException
+    {
+        public TAPDRequiredParameterMissingException(string parameterName) : base(string.Format("Required Parameter Missing:{0}", parameterName))
+        {
+            
+        }
+    }
+}

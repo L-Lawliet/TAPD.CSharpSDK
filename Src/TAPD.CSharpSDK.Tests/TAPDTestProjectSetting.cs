4335dc99ae25fb9a0913c965e563b65b388197cc
LaryLawliet <1577075604@qq.com>
2020-03-28 23:45:53 +0800

    --story=1000067 --user=Lawliet 【功能】增加测试时的项目设置参数（项目ID）

---
+++ Src/TAPD.CSharpSDK.Tests/TAPDTestProjectSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TAPD.CSharpSDK.Tests
+{
+    /// <summary>
+    /// 由于保密原因
+    /// 请填写自己项目中的设置
+    /// </summary>
+    public class TAPDTestProjectSetting
+    {
+        /// <summary>
+        /// 项目ID
+        /// 请填写自己项目的ID
+        /// </summary>
+        public const int workspaceID = 0;
+    }
+}

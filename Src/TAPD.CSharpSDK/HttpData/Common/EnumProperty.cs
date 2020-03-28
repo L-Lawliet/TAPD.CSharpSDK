d632ab64fcef302dfaf86566276afdb750455a21
LaryLawliet <1577075604@qq.com>
2020-03-28 16:39:06 +0800

    --story=1000067 --user=Lawliet 【优化】将枚举属性改成可拓展，增加String和int枚举

--- Src/TAPD.CSharpSDK/HttpData/Common/EnumProperty.cs
+++ Src/TAPD.CSharpSDK/HttpData/Common/EnumProperty.cs
@@ -3,7 +3,51 @@ using System.Collections.Generic;
 
 namespace TAPD.CSharpSDK
 {
-    public class EnumProperty
+    /// <summary>
+    /// 基于整型的枚举
+    /// </summary>
+    public class EnumIntProperty : EnumProperty<int>
+    {
+        public EnumIntProperty(): base()
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enums"></param>
+        public EnumIntProperty(params int[] enums):base(enums)
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// 基于字符串的枚举属性
+    /// </summary>
+    public class EnumStringProperty : EnumProperty<string>
+    {
+        public EnumStringProperty() : base()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enums"></param>
+        public EnumStringProperty(params string[] enums) : base(enums)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 枚举属性基类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EnumProperty<T>
     {
         /// <summary>
         /// 或者的连接符
@@ -13,30 +57,30 @@ namespace TAPD.CSharpSDK
         /// <summary>
         /// 枚举组合
         /// </summary>
-        public List<string> m_Enums;
+        public List<T> m_Enums;
 
         /// <summary>
         /// 
         /// </summary>
         public EnumProperty()
         {
-            m_Enums = new List<string>();
+            m_Enums = new List<T>();
         }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="enums"></param>
-        public EnumProperty(params string[] enums)
+        public EnumProperty(params T[] enums)
         {
-            m_Enums = new List<string>(enums);
+            m_Enums = new List<T>(enums);
         }
 
         /// <summary>
         /// 添加枚举
         /// </summary>
         /// <param name="enumValue"></param>
-        public void AddEnum(string enumValue)
+        public void AddEnum(T enumValue)
         {
             if (!m_Enums.Contains(enumValue))
             {
@@ -48,7 +92,7 @@ namespace TAPD.CSharpSDK
         /// 移除枚举
         /// </summary>
         /// <param name="enumValue"></param>
-        public void RemovePerson(string enumValue)
+        public void RemovePerson(T enumValue)
         {
             m_Enums.Remove(enumValue);
         }
@@ -63,7 +107,7 @@ namespace TAPD.CSharpSDK
 
             if(m_Enums != null && m_Enums.Count > 0)
             {
-                result = StringUtil.Join<string>(OR_CHAR, m_Enums);
+                result = StringUtil.Join<T>(OR_CHAR, m_Enums);
             }
 
             return result;

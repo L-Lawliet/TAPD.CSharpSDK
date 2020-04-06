bc3be8000df6de1c05908db2aa2b6f7620c3cbca
Lawliet <1577075604@qq.com>
2020-04-06 10:41:53 +0800

    --story=1000072 --user=Lawliet 【功能】增加需求计数接口的测试 【功能】使用Setting类常量作为测试参数

--- Src/TAPD.CSharpSDK.Tests/TAPD_Stories_Test.cs
+++ Src/TAPD.CSharpSDK.Tests/TAPD_Stories_Test.cs
@@ -18,7 +18,7 @@ namespace TAPD.CSharpSDK.Tests
         [SetUp]
         public void SetUp()
         {
-            m_Tapd = new TAPD(TAPDTestAuthorization.API_USER, TAPDTestAuthorization.API_PASSWORD, TAPDTestProjectSetting.workspaceID);
+            m_Tapd = new TAPD(TAPDTestAuthorization.API_USER, TAPDTestAuthorization.API_PASSWORD, TAPDTestProjectSetting.WORK_SPACE_ID);
         }
 
         /// <summary>
@@ -31,10 +31,10 @@ namespace TAPD.CSharpSDK.Tests
 
             Assert.AreNotEqual(response.data, null);
             Assert.AreNotEqual(response.data.Length, 0);
-            Assert.AreEqual(response.data[0].workspaceID, TAPDTestProjectSetting.workspaceID.ToString());
+            Assert.AreEqual(response.data[0].workspaceID, TAPDTestProjectSetting.WORK_SPACE_ID.ToString());
 
             //TODO：后续断言与项目需求有关，因此注释掉，可根据需求内容设置
-            //Assert.AreEqual(response.data[0].creator, "XXX");
+            Assert.AreEqual(response.data[0].creator, TAPDTestProjectSetting.CREATOR);
 
         }
 
@@ -43,9 +43,9 @@ namespace TAPD.CSharpSDK.Tests
         /// 需要填写正确的需求ID
         /// </summary>
         /// <param name="id">查询的需求ID</param>
-        [TestCase(100000000)]
-        [TestCase(100000001)]
-        [TestCase(100000002)]
+        [TestCase(TAPDTestProjectSetting.STORY_ID_1)]
+        [TestCase(TAPDTestProjectSetting.STORY_ID_2)]
+        [TestCase(TAPDTestProjectSetting.STORY_ID_3)]
         public void Stories_GetByID_Succeed(long id)
         {
             TAPDStoriesRequest request = new TAPDStoriesRequest();
@@ -56,6 +56,24 @@ namespace TAPD.CSharpSDK.Tests
             Assert.AreNotEqual(response.data, null);
             Assert.AreNotEqual(response.data.Length, 0);
             Assert.AreEqual(response.data[0].id, id.ToString());
+        }
+
+        /// <summary>
+        /// 根据ID查询需求的数量
+        /// </summary>
+        /// <param name="id">查询的需求ID</param>
+        [TestCase(1111111111111111111, ExpectedResult = 0)]  //错误的ID
+        [TestCase(TAPDTestProjectSetting.STORY_ID_1, ExpectedResult = 1)]
+        [TestCase(TAPDTestProjectSetting.STORY_ID_2, TAPDTestProjectSetting.STORY_ID_1, ExpectedResult = 2)]
+        [TestCase(TAPDTestProjectSetting.STORY_ID_1, TAPDTestProjectSetting.STORY_ID_2, TAPDTestProjectSetting.STORY_ID_3, ExpectedResult = 3)]
+        public int StoriesCount_GetByID_Succeed(params long[] ids)
+        {
+            TAPDStoriesCountRequest request = new TAPDStoriesCountRequest();
+            request.id = new IDProperty(ids);
+
+            TAPDResponse<int> response = m_Tapd.RequestStoriesCount(request);
+
+            return response.data;
         }
     }
 }

0cdeda0e7ea2c22f2b7f902db23cf6badd5e648a
Lawliet <1577075604@qq.com>
2020-04-06 10:39:58 +0800

    --story=1000086 --user=Lawliet 【功能】将测试的常用需求、任务、缺陷等ID和名称整合在Setting类 【功能】将创建者名称整合到Setting类里

--- Src/TAPD.CSharpSDK.Tests/TAPDTestProjectSetting.cs
+++ Src/TAPD.CSharpSDK.Tests/TAPDTestProjectSetting.cs
@@ -4,7 +4,8 @@ namespace TAPD.CSharpSDK.Tests
 {
     /// <summary>
     /// 由于保密原因
-    /// 请填写自己项目中的设置
+    /// 请填写自己项目中的设置或数据
+    /// 否则会导致单元测试错误
     /// </summary>
     public class TAPDTestProjectSetting
     {
@@ -12,6 +13,144 @@ namespace TAPD.CSharpSDK.Tests
         /// 项目ID
         /// 请填写自己项目的ID
         /// </summary>
-        public const int workspaceID = 0;
+        public const int WORK_SPACE_ID = 00000000;
+
+        /// <summary>
+        /// 创建者名称
+        /// </summary>
+        public const string CREATOR = "XXX";
+
+        #region 需求
+
+        /// <summary>
+        /// 需求1ID
+        /// </summary>
+        public const long STORY_ID_1 = 1100000000001005179;
+
+        /// <summary>
+        /// 需求2ID
+        /// </summary>
+        public const long STORY_ID_2 = 1100000000001005180;
+
+        /// <summary>
+        /// 需求3ID
+        /// </summary>
+        public const long STORY_ID_3 = 1100000000001005181;
+
+        /// <summary>
+        /// 需求4ID
+        /// </summary>
+        public const long STORY_ID_4 = 1100000000001005182;
+
+        /// <summary>
+        /// 需求1名称
+        /// </summary>
+        public const string STORY_NAME_1 = "test_story_1";
+
+        /// <summary>
+        /// 需求2名称
+        /// </summary>
+        public const string STORY_NAME_2 = "test_story_2";
+
+        /// <summary>
+        /// 需求3名称
+        /// </summary>
+        public const string STORY_NAME_3 = "test_story_3";
+
+        /// <summary>
+        /// 需求4名称
+        /// </summary>
+        public const string STORY_NAME_4 = "test_story_4";
+
+        #endregion
+
+        #region 缺陷
+
+        /// <summary>
+        /// 缺陷1ID
+        /// </summary>
+        public const long BUG_ID_1 = 1100000000001005179;
+
+        /// <summary>
+        /// 缺陷2ID
+        /// </summary>
+        public const long BUG_ID_2 = 1100000000001005180;
+
+        /// <summary>
+        /// 缺陷3ID
+        /// </summary>
+        public const long BUG_ID_3 = 1100000000001005181;
+
+        /// <summary>
+        /// 缺陷4ID
+        /// </summary>
+        public const long BUG_ID_4 = 1100000000001005181;
+
+        /// <summary>
+        /// 缺陷1名称
+        /// </summary>
+        public const string BUG_NAME_1 = "bug_name_1";
+
+        /// <summary>
+        /// 缺陷2名称
+        /// </summary>
+        public const string BUG_NAME_2 = "bug_name_2";
+
+        /// <summary>
+        /// 缺陷3名称
+        /// </summary>
+        public const string BUG_NAME_3 = "bug_name_3";
+
+        /// <summary>
+        /// 缺陷4名称
+        /// </summary>
+        public const string BUG_NAME_4 = "bug_name_4";
+
+        #endregion
+
+        #region 任务
+
+        /// <summary>
+        /// 任务1ID
+        /// </summary>
+        public const long TASK_ID_1 = 1100000000001005179;
+
+        /// <summary>
+        /// 任务2ID
+        /// </summary>
+        public const long TASK_ID_2 = 1100000000001005180;
+
+        /// <summary>
+        /// 任务3ID
+        /// </summary>
+        public const long TASK_ID_3 = 1100000000001005181;
+
+        /// <summary>
+        /// 任务4ID
+        /// </summary>
+        public const long TASK_ID_4 = 1100000000001005181;
+
+
+        /// <summary>
+        /// 任务1名称
+        /// </summary>
+        public const string TASK_NAME_1 = "task_name_1";
+
+        /// <summary>
+        /// 任务2名称
+        /// </summary>
+        public const string TASK_NAME_2 = "task_name_2";
+
+        /// <summary>
+        /// 任务3名称
+        /// </summary>
+        public const string TASK_NAME_3 = "task_name_3";
+
+        /// <summary>
+        /// 任务4名称
+        /// </summary>
+        public const string TASK_NAME_4 = "task_name_4";
+
+        #endregion
     }
 }
